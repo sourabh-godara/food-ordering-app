46234210a3e6233afa5af85fa6e2fbbb93c61757
Sourabh <37789151+Souravgodara@users.noreply.github.com>
2024-02-19 21:27:42 +0530

    Update connectDB.ts

--- src/lib/connectDB.ts
+++ src/lib/connectDB.ts
@@ -10,7 +10,7 @@ export default async function connectDB() {
     await mongoose.connect(connectionString);
     mongoose.connection.on(
       "error",
-      console.error.bind(console, "MongoDB connection error:")
+      console.error.bind(console, "MongoDB connection error")
     );
     mongoose.connection.on("disconnected", connectDB);
   } catch (error) {
